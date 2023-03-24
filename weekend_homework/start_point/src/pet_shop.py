ece613e0ba103538f210272c9102c0928ee47f16
Beth Lupton <bethlupton@martins-mbp.lan>
2023-03-24 15:41:29 +0000

    SecondCommit

--- weekend_homework/start_point/src/pet_shop.py
+++ weekend_homework/start_point/src/pet_shop.py
@@ -7,4 +7,12 @@ def get_total_cash(shop):
     return shop["admin"]["total_cash"]
 
 def add_or_remove_cash(shop,cash):
-    shop["admin"]["total_cash"] + cash
+    shop["admin"]["total_cash"] =  shop["admin"]["total_cash"] + cash
+
+def get_pets_sold(shop):
+    return shop["admin"]["pets_sold"]
+
+def increase_pets_sold(shop, pets_sold):
+     shop["admin"]["pets_sold"] =  shop["admin"]["pets_sold"] + pets_sold
+    
+    
